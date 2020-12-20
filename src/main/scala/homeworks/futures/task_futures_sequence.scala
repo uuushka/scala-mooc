5b687e248bb253a502bae99889b4137eeaaf173b
uuushka <Yushka1988#>
2020-12-20 14:40:05 +0300

    lesson 5

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -1,11 +1,9 @@
 package homeworks.futures
 
-import homeworks.HomeworksUtils.TaskSyntax
-
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
 
 object task_futures_sequence {
-
   /**
    * В данном задании Вам предлагается реализовать функцию fullSequence,
    * похожую на Future.sequence, но в отличии от нее,
@@ -18,6 +16,23 @@ object task_futures_sequence {
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
-  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `encrypt`"()
+  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] = {
+
+    val acc: List[Either[A, Throwable]] = Nil
+    val futureValues = futures
+      .foldRight(acc) { // распределяем вычисления фьючи в Either
+        case (x, acc) => x.value match {
+          case Some(Failure(ex)) => Right(ex) :: acc
+          case Some(Success(value)) => Left(value) :: acc
+          case None => Right(new Exception("Future not completed")) :: acc
+        }
+      }
+      .partitionMap { // делим по Left и Right на кортеж
+        case l@Left(_) => l
+        case r@Right(_) => r
+      }
+
+    Future.successful(futureValues)
+  }
+
 }
